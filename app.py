dc8c068ec068970c720194946167d585af54eb56
Ronak <ronak.vadhaiya@somaiya.edu>
2020-05-18 13:23:14 +0530

    Image generate

--- app.py
+++ app.py
@@ -8,3 +8,13 @@ import numpy as np
 URI = 'http://127.0.0.1:5000'
 
 st.title('Neural Network Visualizer')
+st.sidebar.markdown('## Input Image')
+
+if st.button('Get Random Prediction'):
+  response = requests.post(URI, data={})
+  response = json.loads(response.text)
+  preds = response.get('prediction')
+  image = response.get('image')
+  image = np.reshape(image, (28,28))
+
+  st.image(image, width=150)  
